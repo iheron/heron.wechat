5153959727c400263a9575c1d63165e5147f6703
heron <heron.roman@hotmail.com>
2014-07-10 16:12:00 +0800

    init

---
+++ npm-install.sh
@@ -0,0 +1,5 @@
+cd ./api-server && npm install -d
+echo '============   api-server npm installed ============'
+cd ..
+cd ./web-server && npm install -d
+echo '============   web-server npm installed ============'
