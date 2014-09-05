37087db67d53e3544a6c7597cf304d9a7aa4c8b6
heron <heron.roman@hotmail.com>
2014-09-05 15:39:43 +0800

    init

--- pm2-start.sh
+++ pm2-start.sh
@@ -1,5 +1,5 @@
-cd ./api-server && pm2 start bin/start.js -i max --name "heron.wechat-api-server" --node-args="--harmony NODE_ENV=production"
+cd ./api-server && NODE_ENV=production pm2 start bin/start.js -i max --name "heron.wechat-api-server" --node-args="--harmony"
 echo '============   api-server pm2 started ============'
 cd ..
-cd ./web-server && pm2 start bin/start.js -i max --name "heron.wechat-web-server" --node-args="--harmony NODE_ENV=production"
+cd ./web-server && NODE_ENV=production pm2 start bin/start.js -i max --name "heron.wechat-web-server" --node-args="--harmony"
 echo '============   web-server pm2 started ============'
